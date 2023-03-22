532ec9b5582f57c7933d68fb738b1791f76ea3ec
Earniesty <thanachanok.banj@mail.kmutt.ac.th>
2023-03-22 11:18:53 +0700

    commit message

---
+++ MyProject1.java
@@ -0,0 +1,17 @@
+
+package myproject1;
+
+/**
+ *
+ * @author Dell
+ */
+public class MyProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
